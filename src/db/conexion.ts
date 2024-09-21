c1bc4fd933c10b67359d7d571c7770cb0c077c95
Christian Rafael <crguado@gmail.com>
2024-09-20 23:13:09 -0500

    Usando variables de entorno

--- src/db/conexion.ts
+++ src/db/conexion.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import { DataSource } from "typeorm";
 import { Estudiante } from "../models/estudiantesModel";
 import { Profesor } from "../models/profesoresModel";
@@ -5,10 +6,10 @@ import { Curso } from "../models/cursoModel";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "christian",
-  password: "123",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
   database: "cursos",
   logging: true,
   synchronize: true,
